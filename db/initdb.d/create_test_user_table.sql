e0bf2b542e2e89d2dc0c97f8164842829f826800
Rate980 <ratei.5673@gmail.com>
2022-09-08 16:46:12 +0900

    delete emailとpassword

--- db/initdb.d/create_test_user_table.sql
+++ db/initdb.d/create_test_user_table.sql
@@ -1,9 +1,6 @@
 CREATE TABLE test_user (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    email VARCHAR(128) NOT NULL,
-    passwd_hash CHAR(64) NOT NULL,
-    salt CHAR(4) NOT NULL,
     point INT DEFAULT 0 NOT NULL,
     PRIMARY KEY (id)
 );
--- db/initdb.d/init_test_user.sql
+++ db/initdb.d/init_test_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO test_user (name, email, passwd_hash, salt) VALUES ("tanaka taro", "tanaka.taro@example.com","140afe108eb367be2f34825aacd397e6333dc7689415802e1698253378cd84c9","agro" );
-INSERT INTO test_user (name, email, passwd_hash, salt) VALUES ("yamada hanako", "yamada.hanako@example.com","aa9071bd58344101fe50c1a978ff17df0eef4c0515af76badfc2df094cc7b88e" ,"sdsh");
+INSERT INTO test_user (name) VALUES ("tanaka");
+INSERT INTO test_user (name) VALUES ("yamada");
